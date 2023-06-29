fa8961f1507e19b0361d7f2e7bcc73ef9eaf90f8
ngawang88 <nspunk767@gmail.com>
2023-06-29 10:54:44 +0600

    dependency deleted assets_audio_player

--- lib/pages/model/tts/tts_page.dart
+++ lib/pages/model/tts/tts_page.dart
@@ -11,7 +11,6 @@ import 'package:flutter/material.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:provider/provider.dart';
 import '../../../api/data.dart';
-import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:http/http.dart' as http;
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
